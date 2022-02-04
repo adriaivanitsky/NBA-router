90e8326b71721c231a3d38fbff753058a9900fc6
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-03 22:41:38 -0800

    made a behavior test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 test('renders avatar the last airbender', async () => {
   render(<App />);
@@ -9,3 +10,14 @@ test('renders avatar the last airbender', async () => {
 });
 
 //behavioral tests
+
+//so in this test ima need user clicks on character name, AND, it directs to character detail
+//
+test.only('user can click on character name and be directed to a character detail page', async () => {
+  render(<App />);
+  const name = await screen.findByRole('heading', { name: /46th/i });
+  expect(name).toBeInTheDocument();
+  userEvent.click(name);
+  const details = await screen.findAllByRole('list');
+  expect(details).toHaveLength(2);
+});
